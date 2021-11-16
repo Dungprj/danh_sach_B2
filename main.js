d83eb408ccf0cc1bf81c552226b560fb70e9cf19
Dungprj <89343958+Dungprj@users.noreply.github.com>
2021-11-16 16:20:30 +0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,111 @@
+let Apidb = [{'id': '1', 'msv': '2121050200', 'ho': 'Phạm Long', 'ten': 'An', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0352914185'}, {'id': '2', 'msv': '2121050204', 'ho': 'Đỗ Tuấn', 'ten': 'Anh', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0845508566'}, {'id': '3', 'msv': '2121050197', 'ho': 'Hoàng Tú', 'ten': 'Anh', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0972877637'}, {'id': '4', 'msv': '2121050219', 'ho': 'Hoàng Việt', 'ten': 'Anh', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0359702375'}, {'id': '5', 'msv': '2121050206', 'ho': 'Nguyễn Ngọc', 'ten': 'Bảo', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0964191490'}, {'id': '6', 'msv': '2121050228', 'ho': 'Trần Thành', 'ten': 'Công', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0774328745'}, {'id': '7', 'msv': '2121050238', 'ho': 'Đoàn Văn', 'ten': 'Doanh', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0398296321'}, {'id': '8', 'msv': '2121050246', 'ho': 'Trương Văn', 'ten': 'Duẩn', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0397373190'}, {'id': '9', 'msv': '2121050225', 'ho': 'Đàm Việt', 'ten': 'Dũng', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0962665458'}, {'id': '10', 'msv': '2121050220', 'ho': 'Nguyễn Tiến', 'ten': 'Dũng', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0962988318'}, {'id': '11', 'msv': '2121050244', 'ho': 'Ngô Quốc', 'ten': 'Duy', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0978308003'}, {'id': '12', 'msv': '2121050240', 'ho': 'Phạm Đức', 'ten': 'Duy', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0326638682'}, {'id': '13', 'msv': '2121050203', 'ho': 'Nguyễn Thịnh Tiến', 'ten': 'Dương', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0348707825'}, {'id': '14', 'msv': '2121050241', 'ho': 'Trần Hải', 'ten': 'Dương', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0327998700'}, {'id': '15', 'msv': '2121050199', 'ho': 'Trần Tiến', 'ten': 'Đạt', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0988148010'}, {'id': '16', 'msv': '2121050215', 'ho': 'Nguyễn Trung', 'ten': 'Đức', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0353444983'}, {'id': '17', 'msv': '2121050193', 'ho': 'Nguyễn Việt', 'ten': 'Đức', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0359403598'}, {'id': '18', 'msv': '2121050247', 'ho': 'Lê Trường', 'ten': 'Giang', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0368692003'}, {'id': '19', 'msv': '2121050221', 'ho': 'Nguyễn Thị', 'ten': 'Hải', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0393417014'}, {'id': '20', 'msv': '2121050231', 'ho': 'Trần Hữu', 'ten': 'Hải', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0382654036'}, {'id': '21', 'msv': '2121050212', 'ho': 'Nguyễn Gia', 'ten': 'Hào', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0904777991'}, {'id': '22', 'msv': '2121050253', 'ho': 'Tạ Minh', 'ten': 'Hiển', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0967500372'}, {'id': '23', 'msv': '2121050210', 'ho': 'Nguyễn Minh', 'ten': 'Hiếu', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0965768124'}, {'id': '24', 'msv': '2121050196', 'ho': 'Nguyễn Trung', 'ten': 'Hiếu', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0868701413'}, {'id': '25', 'msv': '2121050234', 'ho': 'Phan Văn', 'ten': 'Hiếu', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0363815701'}, {'id': '26', 'msv': '2121050209', 'ho': 'Vương Trung', 'ten': 'Hiếu', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0977859421'}, {'id': '27', 'msv': '2121050252', 'ho': 'Bùi Huy', 'ten': 'Hoàng', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0342488790'}, {'id': '28', 'msv': '2121050235', 'ho': 'Nguyễn Đức', 'ten': 'Hoàng', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0964796473'}, {'id': '29', 'msv': '2121050251', 'ho': 'Trần Huy', 'ten': 'Hoàng', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0869961617'}, {'id': '30', 'msv': '2121050230', 'ho': 'Bùi Quốc', 'ten': 'Huy', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0326307548'}, {'id': '31', 'msv': '2121050233', 'ho': 'Phạm Quang', 'ten': 'Huy', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0564326534'}, {'id': '32', 'msv': '2121050213', 'ho': 'Vì Quang', 'ten': 'Huy', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0352217696'}, {'id': '33', 'msv': '2121050198', 'ho': 'Nguyễn Thị Thanh', 'ten': 'Hương', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0961867495'}, {'id': '34', 'msv': '2121050249', 'ho': 'Bùi Gia', 'ten': 'Khánh', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0936071203'}, {'id': '35', 'msv': '2121050243', 'ho': 'Nguyễn Xuân', 'ten': 'Khánh', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0386631407'}, {'id': '36', 'msv': '2121050208', 'ho': 'Trần Văn', 'ten': 'Khánh', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0832765582'}, {'id': '37', 'msv': '2121051475', 'ho': 'Ngô Duy', 'ten': 'Khương', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0865497375'}, {'id': '38', 'msv': '2121051491', 'ho': 'Nhữ Tùng', 'ten': 'Lâm', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0923159916'}, {'id': '39', 'msv': '2121050223', 'ho': 'Hoàng Ngọc Bảo', 'ten': 'Long', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0967455138'}, {'id': '40', 'msv': '2121050211', 'ho': 'Nguyễn Lương', 'ten': 'Mạnh', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0348090903'}, {'id': '41', 'msv': '2121050254', 'ho': 'Nguyễn Đỗ Hải', 'ten': 'Minh', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0344856369'}, {'id': '42', 'msv': '2121051507', 'ho': 'Nguyễn Thị', 'ten': 'Mùi', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0379874481'}, {'id': '43', 'msv': '2121050192', 'ho': 'Nguyễn Thị', 'ten': 'Ngát', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0877067591'}, {'id': '44', 'msv': '2121050216', 'ho': 'Nguyễn Đức', 'ten': 'Ngọc', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0352888203'}, {'id': '45', 'msv': '2121051386', 'ho': 'Lương Đắc', 'ten': 'Nhật', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0395833341'}, {'id': '46', 'msv': '2121050226', 'ho': 'Đỗ Anh', 'ten': 'Quân', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0862660783'}, {'id': '47', 'msv': '2121050222', 'ho': 'Mai Anh', 'ten': 'Quân', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0327018867'}, {'id': '48', 'msv': '2121050239', 'ho': 'Sa Minh', 'ten': 'Quân', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0397890853'}, {'id': '49', 'msv': '2121051562', 'ho': 'Phạm Thái', 'ten': 'Sơn', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0846044180'}, {'id': '50', 'msv': '2121050236', 'ho': 'Đinh Tiến', 'ten': 'Thành', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0982183703'}, {'id': '51', 'msv': '2121050245', 'ho': 'Nguyễn Quang', 'ten': 'Thành', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0377195987'}, {'id': '52', 'msv': '2121050195', 'ho': 'Nguyễn Thị Kim', 'ten': 'Thành', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0345380237'}, {'id': '53', 'msv': '2121050248', 'ho': 'Nguyễn Đức', 'ten': 'Thắng', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0866147875'}, {'id': '54', 'msv': '2121050217', 'ho': 'Nguyễn Quốc', 'ten': 'Thắng', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0326127505'}, {'id': '55', 'msv': '2121050250', 'ho': 'Nguyễn Thế', 'ten': 'Thắng', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0396935229'}, {'id': '56', 'msv': '2121050242', 'ho': 'Hoàng Văn', 'ten': 'Toàn', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0362185984'}, {'id': '57', 'msv': '2121050237', 'ho': 'Nguyễn Thị Kiều', 'ten': 'Trang', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0927443149'}, {'id': '58', 'msv': '2121050214', 'ho': 'Bùi Đức', 'ten': 'Trọng', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0896886663'}, {'id': '59', 'msv': '2121050202', 'ho': 'Lê Quyết', 'ten': 'Trường', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0968965064'}, {'id': '60', 'msv': '2121050207', 'ho': 'Trần Văn', 'ten': 'Trường', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0352673539'}, {'id': '61', 'msv': '2121050224', 'ho': 'Bùi Quang', 'ten': 'Tuyến', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0984061814'}, {'id': '62', 'msv': '2121050205', 'ho': 'Đặng Thanh', 'ten': 'Vân', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0967783326'}, {'id': '63', 'msv': '2121050218', 'ho': 'Nguyễn Quốc', 'ten': 'Việt', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0963844428'}, {'id': '64', 'msv': '2121050201', 'ho': 'Lê Ngọc', 'ten': 'Vinh', 'lop': 'DCCTCT66B2', 'nganh': 'Đại học_Công nghệ thông tin_K66B2', 'sdt': '0984882317'}];
+
+
+let getElement_search_btn = document.querySelector("#search_btn")
+
+let getElement_input = document.querySelector("#input_main")
+
+let getElement_show_all = document.querySelector("#show_all_list")
+let getElement_close = document.querySelector("#close_btn")
+
+
+function start(){
+    
+
+    Search_student(Apidb)
+       
+
+
+
+
+
+}
+
+function show_all(){
+    renderstudents(Apidb)
+}
+
+function close_all(){
+    let getElement_list = document.querySelectorAll(".std")
+    getElement_list.forEach((obj)=>{
+        obj.style.display = "none"
+    })
+
+    
+}
+
+// #dùng cho local
+
+// function getstudent(callback){
+//     fetch(Apidb)
+//     .then(function(response){
+//         return response.json()
+//     })
+//     .then(callback);
+// }
+
+
+function renderstudents(data1){
+    var liststudents = document.querySelector("#student");
+    var htmls = 
+    data1.map(function(student){
+        return `<li class="std">
+        <h2 class="content">Số thứ tự :${student.id}</h2>
+        <h3 class="content">Mã sinh viên  :${student.msv}</h3>
+        <h3 class="content">Họ :${student.ho}</h3>
+        <h3 class="content">Tên :${student.ten}</h3>
+        <h3 class="content">Lớp :${student.lop}</h3>
+        <h3 class="content">Ngành :${student.nganh}</h3>
+        <h3 class="content">SĐT :${student.sdt}</h3>
+        </li>`
+    })
+
+    liststudents.innerHTML = htmls.join('');
+
+
+}
+
+
+
+getElement_search_btn.addEventListener("click",start)
+getElement_show_all.addEventListener("click",show_all)
+getElement_close.addEventListener("click",close_all)
+
+function Search_student(data){
+    
+
+
+    data_inp = getElement_input.value
+
+    
+
+    let result_search = data.filter(obj=>{
+        return obj.msv == data_inp
+        
+    
+    })
+
+    if (result_search){
+        renderstudents(result_search)
+
+    }else(
+        alert('Số báo danh không tồn tại')
+        
+    )
+
+
+    
+
+
+    
+
+}
+
+
+
+
+
+
+// start()
+
+
